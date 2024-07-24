7cb7624866fa700b5cc4f82af6dc6b8a7a1fcf55
Rodrigo Bestard Pino <pacobpino@gmail.com>
2024-07-24 12:17:12 -0300

    base styling and componetns

--- apps/admin-main/src/app/page.tsx
+++ apps/admin-main/src/app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { Button } from "@wg-frontend/ui/button";
+
 export default function HomePage() {
-  return <main className="container h-screen py-16">Hello world</main>;
+  return (
+    <main className="container h-screen py-16">
+      Hello world
+      <Button variant="destructive">Click me</Button>
+    </main>
+  );
 }
