c646fc015f4465d2e0bcf4e58b50eda985c759e3
Jessica Sandoval <jessica.sandoval@scrummers.co>
2024-07-30 10:26:36 -0500

    fix: next config error

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,11 +1,6 @@
 import { fileURLToPath } from "url";
 import createJiti from "jiti";
 
-<<<<<<< HEAD:apps/admin/next.config.js
-=======
-const { ADMIN_AUTH_URL, ADMIN_USERS_URL } = process.env;
-
->>>>>>> 04abd4b (initial commit  users managment micro frontend):apps/admin-main/next.config.js
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
 createJiti(fileURLToPath(import.meta.url))("./src/env");
 
@@ -19,31 +14,6 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-<<<<<<< HEAD:apps/admin/next.config.js
-=======
-
-  // Map microfrontends to their respective paths
-  rewrites: async () => {
-    return [
-      {
-        source: "/auth",
-        destination: `${ADMIN_AUTH_URL}/`,
-      },
-      {
-        source: "/auth/:path*",
-        destination: `${ADMIN_AUTH_URL}/:path*`,
-      },
-      {
-        source: "/users",
-        destination: `${ADMIN_USERS_URL}/`,
-      },
-      {
-        source: "/users",
-        destination: `${ADMIN_USERS_URL}/`,
-      },
-    ];
-  },
->>>>>>> 04abd4b (initial commit  users managment micro frontend):apps/admin-main/next.config.js
 };
 
 export default config;
