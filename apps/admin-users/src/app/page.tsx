6c149964c357802d37afe2ee94d98e1077ddea55
Jessica Sandoval <jessica.sandoval@scrummers.co>
2024-07-30 09:20:01 -0500

    fix conflict files

--- apps/admin-users/src/app/page.tsx
+++ apps/admin-users/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+<<<<<<< HEAD
 import { PasswordInput } from '@wg-frontend/ui/password-input';
 
 export default function HomePage() {
@@ -8,6 +9,12 @@ export default function HomePage() {
       <div className='w-full px-10'>
         <PasswordInput />
       </div>   
+=======
+export default function HomePage() {
+  return (
+    <main className="container h-screen py-16">
+      Hello world from users microfrontend
+>>>>>>> 04abd4b (initial commit  users managment micro frontend)
     </main>
   );
 }
