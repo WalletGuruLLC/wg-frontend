a46cf037ae2c2741d8b18923beb112ed2cdd6ca2
Rodrigo Bestard Pino <pacobpino@gmail.com>
2024-08-10 21:55:04 -0300

    documented custom fetch function

--- apps/admin/src/lib/data-access/custom-fetch.ts
+++ apps/admin/src/lib/data-access/custom-fetch.ts
@@ -1,3 +1,14 @@
+/**
+ * customFetch is a wrapper around the native fetch function that adds the Authorization header to the request.
+ * It also allows you to specify the response type as a generic parameter and automatically parses the response as JSON.
+ *
+ * @param params The same parameters as the native fetch function.
+ * @returns A promise that resolves to the parsed JSON response.
+ *
+ * @example
+ * const data = await customFetch<{ name: string }>("https://api.example.com/user-data/123");
+ * console.log(data.name);
+ */
 export default function customFetch<ResponseType>(
   ...params: Parameters<typeof fetch>
 ) {
