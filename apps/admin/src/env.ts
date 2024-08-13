897f4f3b8fefbf9b759535c969738d947c0bb1bf
Rodrigo Bestard Pino <pacobpino@gmail.com>
2024-08-13 09:10:46 -0300

    added backend urls env

--- apps/admin/src/env.ts
+++ apps/admin/src/env.ts
@@ -5,7 +5,8 @@ import { z } from "zod";
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_ADMIN_BASE_URL: z.string().min(1),
-    NEXT_PUBLIC_MICROSERVICE1_URL: z.string().min(1),
+    NEXT_PUBLIC_AUTH_MICROSERVICE_URL: z.string().min(1),
+    NEXT_PUBLIC_NOTIFICATION_MICROSERVICE_URL: z.string().min(1),
   },
   shared: {
     NODE_ENV: z
@@ -14,7 +15,10 @@ export const env = createEnv({
   },
   runtimeEnv: {
     NEXT_PUBLIC_ADMIN_BASE_URL: process.env.NEXT_PUBLIC_ADMIN_BASE_URL,
-    NEXT_PUBLIC_MICROSERVICE1_URL: process.env.NEXT_PUBLIC_MICROSERVICE1_URL,
+    NEXT_PUBLIC_AUTH_MICROSERVICE_URL:
+      process.env.NEXT_PUBLIC_AUTH_MICROSERVICE_URL,
+    NEXT_PUBLIC_NOTIFICATION_MICROSERVICE_URL:
+      process.env.NEXT_PUBLIC_NOTIFICATION_MICROSERVICE_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
   skipValidation:
