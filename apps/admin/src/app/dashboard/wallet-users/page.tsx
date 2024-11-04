33eba5c2a54a1b23243bb1e9b0fcf9174e30b818
Jessica Sandoval <jessica.sandoval@scrummers.co>
2024-11-04 17:13:49 -0500

    add filter options

--- apps/admin/src/app/dashboard/wallet-users/page.tsx
+++ apps/admin/src/app/dashboard/wallet-users/page.tsx
@@ -250,19 +250,15 @@ const columns = [
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Link href={`/dashboard/wallet-users/${idUser}`}>
-                  <Button
-                    type="button"
-                    variant="ghost"
-                    size="sm"
-                    className="hover:bg-transparent hover:text-[#3678B1]"
-                  >
-                    <ChevronRight
-                      stroke="#3678B1"
-                      strokeWidth={0.75}
-                      className="size-6 font-semibold"
-                    />
-                  </Button>
+                <Link
+                  className="text-[#3678B1]"
+                  href={`/dashboard/wallet-users/${idUser}`}
+                >
+                  <ChevronRight
+                    stroke="#3678B1"
+                    strokeWidth={0.75}
+                    className="size-6 font-semibold"
+                  />
                 </Link>
               </TooltipTrigger>
               <TooltipContent>{tooltip}</TooltipContent>
