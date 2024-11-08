5e849cf49307147257d86a31ec701deadc7ff8e2
Jessica Sandoval <jessica.sandoval@scrummers.co>
2024-11-08 15:15:55 -0500

    update texts wallet-users

--- apps/admin/src/lib/validators/index.ts
+++ apps/admin/src/lib/validators/index.ts
@@ -279,8 +279,3 @@ export const walletusersValidator = z.object({
   state: z.string().min(1),
   wallet: z.string().min(1),
 });
-
-export const walletusersValidator = z.object({
-  state: z.string().min(1),
-  wallet: z.string().min(1),
-});
