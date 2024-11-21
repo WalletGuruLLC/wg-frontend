108b665fe550e9c7b60c64529b3577b47cd36a2a
Jessica Sandoval <jessica.sandoval@scrummers.co>
2024-11-21 15:50:42 -0500

    button activate refund and dispute from details

--- apps/admin/src/app/dashboard/reports/transactions-by-user/page.tsx
+++ apps/admin/src/app/dashboard/reports/transactions-by-user/page.tsx
@@ -58,12 +58,14 @@ import { SimpleTitle } from "../../_components/dashboard-title";
 
 function Actions({ activity }: { activity: Activity }) {
   const { values } = useI18n();
+  const userData = useGetAuthedUserInfoQuery(undefined);
 
   return (
     <div className="flex flex-row space-x-4">
       {activity.activityId && (
         <DetailsDialog
           activity={activity}
+          typeUser={userData.data?.type ?? "PROVIDER"}
           trigger={
             <Button
               className="flex h-max flex-row items-center space-x-2"
@@ -670,7 +672,11 @@ const columnsDetails = [
   }),
 ];
 
-function DetailsDialog(props: { activity: Activity; trigger: ReactNode }) {
+function DetailsDialog(props: {
+  activity: Activity;
+  typeUser: string;
+  trigger: ReactNode;
+}) {
   const { values } = useI18n();
   //const errors = useErrors();
   const [isOpen, _, __, toggle] = useBooleanHandlers();
