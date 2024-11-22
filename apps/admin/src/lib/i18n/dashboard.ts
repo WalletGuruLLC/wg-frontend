fecafa33e2d9523a94559d0b5c6b2e26cf49135d
Jessica Sandoval <jessica.sandoval@scrummers.co>
2024-11-22 17:14:55 -0500

    fix lint

--- apps/admin/src/lib/i18n/dashboard.ts
+++ apps/admin/src/lib/i18n/dashboard.ts
@@ -865,6 +865,5 @@ export const dashboardDict = {
     "dashboard.reports.sections.transactions-by-provider.details.date": "Fecha",
     "dashboard.dispute.button.details": "Disputa",
     "dashboard.refund.button.details": "Reembolso",
-    "dashboard.roles.role.modules.paymentSummary": "Pagos recibidos",
   },
 } satisfies I18nDictionary;
