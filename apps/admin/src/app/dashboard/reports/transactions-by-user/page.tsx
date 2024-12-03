92169728c1fb42bef5bcc8382f150d78d9dc9896
Cristian <christian.dulcey@gmail.com>
2024-12-02 20:47:23 -0500

    fix error in transaction by user

--- apps/admin/src/app/dashboard/reports/transactions-by-user/page.tsx
+++ apps/admin/src/app/dashboard/reports/transactions-by-user/page.tsx
@@ -501,31 +501,55 @@ export default function TransactionsByUserPage() {
                   {
                     values[
                       "dashboard.reports.sections-transactions-by-user.search.provider.label"
-                    ]
+                      ]
                   }
                 </Label>
-                <SelectContent>
-                  {providersData?.providers
-                    .filter((p) =>
+                <Select
+                  onValueChange={(value) =>
+                    handleFiltersChange({
+                      ...filters,
+                      providerIds: value,
+                    })
+                  }
+                  defaultValue={filters.providerIds}
+                >
+                  <SelectTrigger
+                    className={cn(
+                      "rounded-lg border border-black",
+                      !filters.providerIds && "text-gray-400",
+                    )}
+                  >
+                    <SelectValue
+                      placeholder={
+                        values[
+                          `dashboard.reports.sections-transactions-by-user.search.provider.placeholder`
+                          ]
+                      }
+                    />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {providersData?.providers
+                      .filter((p) =>
+                        accessLevelsData?.providers[
+                          p.id
+                          ]?.transactionsByUser.includes("view"),
+                      )
+                      .map((provider) => (
+                        <SelectItem key={provider.id} value={provider.id}>
+                          {provider.name}
+                        </SelectItem>
+                      ))}
+                    {providersData?.providers.filter((p) =>
                       accessLevelsData?.providers[
                         p.id
-                      ]?.transactionsByUser.includes("view"),
-                    )
-                    .map((provider) => (
-                      <SelectItem key={provider.id} value={provider.id}>
-                        {provider.name}
+                        ]?.transactionsByUser.includes("view"),
+                    ).length === 0 && (
+                      <SelectItem value="no" disabled>
+                        No providers available
                       </SelectItem>
-                    ))}
-                  {providersData?.providers.filter((p) =>
-                    accessLevelsData?.providers[
-                      p.id
-                    ]?.transactionsByUser.includes("view"),
-                  ).length === 0 && (
-                    <SelectItem value="no" disabled>
-                      No providers available
-                    </SelectItem>
-                  )}
-                </SelectContent>
+                    )}
+                  </SelectContent>
+                </Select>
               </div>
             )
           }
