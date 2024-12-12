18740bb77ac1640addea7fed04d29396165d7ad3
Jessica Sandoval <jessica.sandoval@scrummers.co>
2024-12-12 18:02:09 -0500

    add filters

--- apps/admin/src/app/dashboard/dispute/page.tsx
+++ apps/admin/src/app/dashboard/dispute/page.tsx
@@ -9,16 +9,17 @@ import {
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { Search } from "lucide-react";
+import { format } from "date-fns";
+import { CalendarIcon, Search } from "lucide-react";
 
 import { cn } from "@wg-frontend/ui";
+import { Form, FormField, FormMessage, useForm } from "@wg-frontend/ui/form";
+import { Label } from "@wg-frontend/ui/label";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormMessage,
-  useForm,
-} from "@wg-frontend/ui/form";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@wg-frontend/ui/popover";
 import {
   Select,
   SelectContent,
@@ -33,26 +34,23 @@ import { Button } from "~/components/button";
 import { SelectTrigger } from "~/components/select";
 import {
   useGetAuthedUserAccessLevelsQuery,
-  // useGetAuthedUserInfoQuery,
+  useGetAuthedUserInfoQuery,
   useGetListDisputesQuery,
+  useGetProvidersQuery,
   useGetSettingQuery,
 } from "~/lib/data-access";
-//import { useErrors } from "~/lib/data-access/errors";
 import { useAccessLevelGuard } from "~/lib/hooks";
 import { useI18n } from "~/lib/i18n";
 import { disputeValidator } from "~/lib/validators";
-//import ConfirmDialog from "../_components/dashboard-confirm-dialog";
-//import Dialog from "../_components/dashboard-dialog";
+import { Calendar } from "../_components/dashboard-calendar";
 import { FormItem, FormLabel } from "../_components/dashboard-form";
 import { Input } from "../_components/dashboard-input";
-//import { Switch } from "../_components/dashboard-switch";
 import Table, {
   ColumnHeader,
   PaginationFooter,
 } from "../_components/dashboard-table";
 import { SimpleTitle } from "../_components/dashboard-title";
 
-//const COUNTDOWN_TIME = 60 * 5; // 5 minutes
 const formatCurrency = (value: number, code: string, scale: number) => {
   const formattedValue = new Intl.NumberFormat("en-US", {
     minimumFractionDigits: scale,
@@ -108,28 +106,6 @@ const columns = [
     },
     header: () => <ColumnHeader i18nKey="list-disputes-table-amount" />,
   }),
-  /*
-  columnHelper.accessor(
-    () => {
-      const formatDate = (date: Date) => {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, "0");
-        const day = String(date.getDate()).padStart(2, "0");
-        const hours = String(date.getHours()).padStart(2, "0");
-        const minutes = String(date.getMinutes()).padStart(2, "0");
-        const seconds = String(date.getSeconds()).padStart(2, "0");
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-      };
-      const currentDate = new Date();
-      const formattedDate = formatDate(currentDate);
-      return formattedDate;
-    },
-    {
-      id: "timestamp",
-      cell: (info) => info.getValue(),
-      header: () => <ColumnHeader i18nKey="wallet-users.table.header.time" />,
-    },
-  ),*/
 ];
 
 export default function ListDisputesPage() {
@@ -146,25 +122,41 @@ export default function ListDisputesPage() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
-
+  const { data: accessLevelsData, isLoading: isLoadingAccessLevels } =
+    useGetAuthedUserAccessLevelsQuery(undefined);
+  const { data: userData } = useGetAuthedUserInfoQuery(undefined);
+  const { data: providersData } = useGetProvidersQuery(
+    {
+      items: "10",
+      type: "PLATFORM",
+    },
+    {
+      enabled: !isLoadingAccessLevels,
+    },
+  );
   const paginationAndSearch: z.infer<typeof paginationAndSearchValidator> = {
     page: searchParams.get("page") ?? "1",
     items: searchParams.get("items") ?? "10",
     search: searchParams.get("search") ?? "",
   };
-  const defaultValues = { description: "", walletAddress: "" };
+  const defaultValues = {
+    walletAddress: "",
+    serviceProviderId: searchParams.get("serviceProviderId") ?? "",
+    startDate: searchParams.get("startDate")
+      ? new Date(Number(searchParams.get("startDate")))
+      : undefined,
+    endDate: searchParams.get("endDate")
+      ? new Date(Number(searchParams.get("endDate")))
+      : undefined,
+  };
   const [filters, setFilters] = useState(defaultValues);
   const [tempFilters, setTempFilters] = useState(defaultValues);
   const [filtered, setFiltered] = useState(false);
-  /* const { data: dataUser, isLoading: userIsLoading } =
-    useGetAuthedUserInfoQuery(undefined);*/
 
   const { data, isLoading } = useGetListDisputesQuery({
     ...paginationAndSearch,
     ...filters,
   });
-  const { data: accessLevelsData, isLoading: isLoadingAccessLevels } =
-    useGetAuthedUserAccessLevelsQuery(undefined);
   const disputesData = (data?.disputes ?? []).map((dispute) => {
     return {
       createDate: dispute.createDate,
@@ -178,13 +170,12 @@ export default function ListDisputesPage() {
       wallet: dispute.wallet?.replace(rootWallet?.value ?? "", "") ?? "-",
     };
   });
-
   const table = useReactTable({
     data: disputesData,
     columns: columns.filter(
       (c) =>
         c.id !== "active" ||
-        accessLevelsData?.general.users.includes("inactive"),
+        accessLevelsData?.general.disputes.includes("inactive"),
     ),
     getCoreRowModel: getCoreRowModel(),
     manualPagination: true,
@@ -227,8 +218,8 @@ export default function ListDisputesPage() {
         title={`${values["list-disputes-title"]}`}
         showLoadingIndicator={isLoading}
       />
-      <div className="flex flex-row items-center space-x-6">
-        <div className="relative w-1/2">
+      <div className="flex flex-row items-center space-x-3">
+        <div className="relative w-1/3">
           <Input
             placeholder={values["wallet-users.search.placeholder"]}
             className="rounded-full border border-black"
@@ -248,7 +239,7 @@ export default function ListDisputesPage() {
             strokeWidth={0.75}
           />
         </div>
-        <div className="relative -top-4 w-1/2">
+        <div className="relative -top-4 w-2/3">
           <Form {...form}>
             <form
               onSubmit={(e) => {
@@ -256,52 +247,178 @@ export default function ListDisputesPage() {
               }}
             >
               <div className="flex w-full flex-row flex-wrap space-x-2">
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem className="flex-1">
-                      <FormLabel className="text-gray-400">
-                        {values["wallet-users.table.header.wallet"]}
-                      </FormLabel>
-                      <Select
-                        value={tempFilters.description}
-                        onValueChange={(value) => {
-                          field.onChange(value);
-                          setTempFilters((prev) => ({
-                            ...prev,
-                            wallet: value,
-                          }));
-                        }}
-                        defaultValue={field.value}
+                <div className="flex flex-col space-y-1 self-end">
+                  <Label className="font-normal">
+                    {
+                      values[
+                        "dashboard.reports.sections-transactions-by-user.search.start-date.label"
+                      ]
+                    }
+                  </Label>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        className={cn(
+                          "relative h-11 min-w-44 justify-start rounded-lg border border-black pl-3 text-left text-sm font-normal",
+                          !filters.startDate && "text-[#A1A1A1]",
+                        )}
                       >
-                        <FormControl>
-                          <SelectTrigger
-                            className={cn(
-                              "rounded-none border-transparent border-b-black",
-                            )}
-                          >
-                            <SelectValue
-                              placeholder={values["wallet-users.select-wallet"]}
-                            />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value={"unlock"}>
-                            {values["wallet-users.active-wallet"]}
-                          </SelectItem>
-                          <SelectItem value={"lock"}>
-                            {values["wallet-users.locked-wallet"]}
-                          </SelectItem>
-                          <SelectItem value={"noWallet"}>
-                            {values["wallet-users.no-wallet"]}
-                          </SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+                        {filters.startDate ? (
+                          format(filters.startDate, "yyyy-MM-dd")
+                        ) : (
+                          <span>yyyy-mm-dd</span>
+                        )}
+                        <CalendarIcon
+                          className="absolute right-2 size-5"
+                          strokeWidth={0.95}
+                        />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={filters.startDate}
+                        onSelect={(date) =>
+                          setFilters((prev) => ({
+                            ...prev,
+                            startDate: date ?? undefined,
+                          }))
+                        }
+                        disabled={[
+                          {
+                            after: filters.endDate
+                              ? filters.endDate
+                              : new Date(),
+                          },
+                        ]}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </div>
+                <div className="flex flex-col space-y-1 self-end">
+                  <Label className="font-normal">
+                    {
+                      values[
+                        "dashboard.reports.sections-transactions-by-user.search.end-date.label"
+                      ]
+                    }
+                  </Label>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        className={cn(
+                          "relative h-11 min-w-44 justify-start rounded-lg border border-black pl-3 text-left text-sm font-normal",
+
+                          !filters.endDate && "text-[#A1A1A1]",
+                        )}
+                      >
+                        {filters.endDate ? (
+                          format(filters.endDate, "yyyy-MM-dd")
+                        ) : (
+                          <span>yyyy-mm-dd</span>
+                        )}
+                        <CalendarIcon
+                          className="absolute right-2 size-5"
+                          strokeWidth={0.95}
+                        />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={filters.endDate}
+                        onSelect={(date) =>
+                          setFilters((prev) => ({
+                            ...prev,
+                            endDate: date ?? undefined,
+                          }))
+                        }
+                        disabled={
+                          filters.startDate
+                            ? [{ before: filters.startDate }]
+                            : []
+                        }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </div>
+                {
+                  /* Provider */
+                  userData?.type === "PLATFORM" && (
+                    <div className="min-w-60 flex-1 space-y-0">
+                      <FormField
+                        control={form.control}
+                        name="description"
+                        render={() => (
+                          <FormItem className="flex-1">
+                            <FormLabel className="text-gray-400">
+                              {
+                                values[
+                                  "dashboard.reports.sections-transactions-by-user.search.provider.label"
+                                ]
+                              }
+                            </FormLabel>
+
+                            <Select
+                              onValueChange={(value) =>
+                                setFilters((prev) => ({
+                                  ...prev,
+                                  serviceProviderId: value,
+                                }))
+                              }
+                              defaultValue={filters.serviceProviderId}
+                            >
+                              <SelectTrigger
+                                className={cn(
+                                  "rounded-none border-transparent border-b-black",
+                                  !filters.serviceProviderId && "text-gray-400",
+                                )}
+                              >
+                                <SelectValue
+                                  placeholder={
+                                    values[
+                                      `dashboard.reports.sections-transactions-by-user.search.provider.placeholder`
+                                    ]
+                                  }
+                                />
+                              </SelectTrigger>
+                              <SelectContent>
+                                {providersData?.providers
+                                  .filter((p) =>
+                                    accessLevelsData?.providers[
+                                      p.id
+                                    ]?.disputes.includes("view"),
+                                  )
+                                  .map((provider) => (
+                                    <SelectItem
+                                      key={provider.id}
+                                      value={provider.id}
+                                    >
+                                      {provider.name}
+                                    </SelectItem>
+                                  ))}
+                                {providersData?.providers.filter((p) =>
+                                  accessLevelsData?.providers[
+                                    p.id
+                                  ]?.disputes.includes("view"),
+                                ).length === 0 && (
+                                  <SelectItem value="no" disabled>
+                                    No providers available
+                                  </SelectItem>
+                                )}
+                              </SelectContent>
+                            </Select>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                  )
+                }
                 {/*
                 <FormField
                   control={form.control}
